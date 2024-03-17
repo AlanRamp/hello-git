dd82d0b4c6f3b1e17ad4d3386c32f993c249e680
alanRam <alan.ramirez4817@alumnos.udg.mx>
2024-03-17 12:52:11 -0600

    Fase 1 login

--- user.c
+++ user.c
@@ -1,7 +1,29 @@
 #include <stdio.h> 
 
 
+
 int main(){
+  struct user
+  {
+    int id;
+    char name[50];
+    char password[30];
+    int user_num;
+  }user_1;
+  
+  printf("Ingrese su nombre: ");
+  scanf("%s", user_1.name);
+  printf("Ingrese su contraseña: ");
+  scanf("%s", user_1.password);
+  printf("Ingrese su id: ");
+  scanf("%d", &user_1.id);
+  user_1.user_num=1;
+  
+  FILE *fp; 
+  fp = fopen("user_data", "r+");
+  fwrite( user_1.name, sizeof(char), sizeof(user_1.name), fp );
+  
+  fclose(fp);
 
 
 
